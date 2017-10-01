3cdf393ecd2383d9d1db3ff6346f242da3aaf8db
Stefan CENUSA <scenusa@bitdefender.com>
2017-10-01 16:44:26 +0300

    added lab1 things

--- src/pages/Lab1Page.vue
+++ src/pages/Lab1Page.vue
@@ -2,49 +2,38 @@
     <v-container>
         <h3 class="my-3">Laboratorul 1</h3>
 
-        <!--<h4>Comenzi</h4>-->
-        <!--<div class="text-comanda">-->
-            <!--<p>Forma generala de lansare în executie a unei comenzi:</p>-->
-            <!--<code class="px-1 py-1">$ comanda [optiuni] [argumente]</code>-->
-        <!--</div>-->
+        <h4>Comenzi</h4>
+        <div class="text-comanda">
+            <p>Forma generala de lansare în executie a unei comenzi:</p>
+            <code class="px-1 py-1">$ comanda [optiuni] [argumente]</code>
+        </div>
 
-        <!--<div class="text-comanda">-->
-            <!--<p>Afiseaza pagina de help pentru o comanda specificata:</p>-->
-            <!--<code class="px-1 py-1">$ help nume_comanda_interna</code>-->
-        <!--</div>-->
-
-        <!--&lt;!&ndash;<p>&ndash;&gt;-->
-            <!--&lt;!&ndash;Pentru a obtine help despre comenzile externa (si despre functiile de biblioteca C)&ndash;&gt;-->
-            <!--&lt;!&ndash;sunt disponibile urmatoarele comenzi:&ndash;&gt;-->
-            <!--&lt;!&ndash;<strong>man, whatis, whereis, apropos, info</strong>&ndash;&gt;-->
-        <!--&lt;!&ndash;</p>&ndash;&gt;-->
+        <p>
+            Pentru a obtine help despre comenzile externa (si despre functiile de biblioteca C)
+            sunt disponibile urmatoarele comenzi:
+            <strong>man, whatis, whereis, apropos, info</strong>
+        </p>
 
 
-        <!--<div class="text-comanda">-->
-            <!--<p>Afiseaza pagina de manual (din sectiunea specificata) pentru comanda sau functia specificata:</p>-->
-            <!--<code class="px-1 py-1">$ man [sectiune] nume</code>-->
-        <!--</div>-->
+        <div class="text-comanda">
+            <p>Afiseaza pagina de manual (din sectiunea specificata) pentru comanda sau functia specificata:</p>
+            <code class="px-1 py-1">$ man [sectiune] nume</code>
+        </div>
 
 
-        <!--<div class="text-comanda">-->
-            <!--<p>Afiseaza locatia comenzii si a paginii de manual asociate</p>-->
-            <!--<code class="px-1 py-1">$ whereis nume</code>-->
-        <!--</div>-->
+        <div class="text-comanda">
+            <p>Compilarea unu program C se face cu comanda</p>
+            <code class="px-1 py-1">$ gcc surca.c [- o executabil]</code>
+        </div>
 
 
-        <!--<div class="text-comanda">-->
-            <!--<p>Compilarea unu program C se face cu comanda</p>-->
-            <!--<code class="px-1 py-1">$ gcc surca.c [- o executabil]</code>-->
-        <!--</div>-->
+        <div class="text-comanda">
+            <p>Compilarea unu program C++ se face cu comanda</p>
+            <code class="px-1 py-1">$ g++ surca.cpp [- o executabil]</code>
+        </div>
 
 
-        <!--<div class="text-comanda">-->
-            <!--<p>Compilarea unu program C++ se face cu comanda</p>-->
-            <!--<code class="px-1 py-1">$ g++ surca.cpp [- o executabil]</code>-->
-        <!--</div>-->
-
-
-        <!--<p class="note">Pentru depanarea programelor se poate utiliza depanatorul GNU DeBugger, accesibil prin comanda <strong>gdb</strong>.</p>-->
+        <p class="note">Pentru depanarea programelor se poate utiliza depanatorul GNU DeBugger, accesibil prin comanda <strong>gdb</strong>.</p>
 
     </v-container>
 </template>
