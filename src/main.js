5dc09b3e6e328dee0d7327ddf8326b02af3988d8
Stefan CENUSA <scenusa@bitdefender.com>
2017-10-02 11:46:02 +0300

    added analytics

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import VueRouter from 'vue-router';
+import VueAnalytics from 'vue-analytics'
 import App from './App.vue';
 import HomePage from './pages/HomePage.vue';
 import Lab1Page from './pages/Lab1Page.vue';
@@ -30,6 +31,11 @@ const router = new VueRouter({
 
 Vue.use(Vuetify);
 Vue.use(VueRouter);
+
+Vue.use(VueAnalytics, {
+    id: 'UA-107386321-1',
+    router
+});
 
 new Vue({ // eslint-disable-line no-new
   el: '#app',
