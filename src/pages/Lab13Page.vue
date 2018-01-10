c3b4ccaefee7b409d2f316a81c6a5205e6ef9e72
Ștefan Cenușă <cenusa.s@gmail.com>
2018-01-10 07:57:18 +0200

    added inscriere proiecte

--- src/pages/Lab13Page.vue
+++ src/pages/Lab13Page.vue
@@ -2,6 +2,14 @@
     <v-container>
         <h3 class="my-3">Laboratorul 13</h3>
 
+        <p>
+            Va rog completati <strong><a href="https://docs.google.com/spreadsheets/d/1UQDZZbO4numiDyUFf7dkKE7bcnx5QLdP_ZXVP4dMTgw/edit?usp=sharing">spreadsheetul</a></strong> pentru stabilirea ordinei de prezentare a proiectelor in S13, respectiv S14.
+            Pentru a nu exista probleme blocati linia pe care v-ati trecut.
+            <br/>
+            Prioritate la prezentare vor avea studentii inscrisi, in ordinea completata.
+        </p>
+        <br/>
+        <br/>
         <p>Prezentare proiecte (punct bonus)</p>
 
         <h6 class="mt-5">Bibliografie</h6>
